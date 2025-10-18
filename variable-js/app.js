3b1a91f6a7a3eb8b0e7cb64f98260f71a3d017f5
Tanbin Akter <tanbinakter.cse@gmail.com>
2025-10-18 07:15:23 +0600

    this is javascript problem solving 2

---
+++ variable-js/app.js
@@ -0,0 +1,59 @@
+let myName ="Tanbin";
+let myAge = 22;
+let isStudent = true;
+let frvLanguage = [ "javascript, phython, c++, csharp"];
+
+let myAddress = {
+    country: "bangladesh",
+    district: "jashore"
+}
+
+
+console.log(myName);
+console.log(myAge);
+console.log(isStudent);
+console.log(frvLanguage);
+console.log(myAddress);
+
+// this is addition result
+let num1 = 15;
+let num2 = 4;
+let addition = num1 + num2 ;
+console.log(addition);
+
+// this is subtraction result
+let subtraction =num1 - num2;
+console.log(subtraction);
+
+// this is multiplication
+let multiplication =num1 * num2;
+console.log(multiplication);
+// this is divition result
+let divition = num1/num2;
+console.log(divition);
+
+
+// calculation grading point 
+let marks = 20;
+
+if(marks >= 80){
+    console.log("grade: A+");
+}
+else if(marks >= 70){
+    console.log("grade: A");
+}
+else if(marks >= 60){
+    console.log("grade: A-");
+}
+else if(marks >= 50){
+    console.log("grade: b");
+}
+else if(marks >= 40){
+    console.log("grade: c");
+}
+else if(marks >= 33){
+    console.log("grade: d");
+} else{
+    console.log("grade: f(fail)");
+}
+
