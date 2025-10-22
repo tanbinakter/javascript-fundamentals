b1d18299105ea2227400a231095ee449a74f409c
Tanbin Akter(tanbi) <tanbinakter.cse@gmail.com>
2025-10-22 08:55:20 +0600

    Add files via upload

---
+++ style.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+const heading = document.getElementById('heading');
+
+
+function redMe(){
+  heading.innerHTML=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas repellendus placeat velit, facere, dolore illo necessitatibus eligendi pariatur cum officiis incidunt quis quidem ducimus? Earum ipsum neque magnam nulla undelorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit inventore commodi, exercitationem officia, veniam unde itaque assumenda debitis molestiae dolore accusamus fugiat quibusdam voluptatum iste, ad ipsam sint amet et..";
+
+  heading.style.color = "blue";
+  
+  heading.style.width='400px'
+  heading.style.height='250px'
+  heading.style.border='3px solid black'
+
+}
+
+
+// console.log("heading", text);
